9d97f6ad91ba5fefeeaf120e62fe02006872ebf1
wuhk76 <wuhk76@gmail.com>
2026-04-12 16:43:34 -0700

    Add files via upload

--- esplora.py
+++ esplora.py
@@ -2,14 +2,16 @@ import serial
 class Esplora:
     def __init__(self, port, baud, timeout = 1):
         self.esp = serial.Serial(port, baud, timeout = timeout)
-    def send(self, data):
+    def send(self, data, encoding = 'utf-8'):
         stx = bytes([0x02])
         size = len(data).to_bytes(4, byteorder = 'big')
+        data = data.encode(encoding) if encoding else data
         self.esp.write(stx + size + data)
-    def receive(self):
+    def receive(self, decoding = 'utf-8'):
         etx = bytes([0x03])
         self.esp.write(etx)
         size = self.esp.read(4)
         size = int.from_bytes(size, byteorder = 'big')
         data = self.esp.read(size)
+        data = data.decode(decoding) if decoding else data
         return data
